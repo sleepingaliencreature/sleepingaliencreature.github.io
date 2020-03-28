bab743ad15a9bdc67d98600771779a43b647988a
Дмитрий Баранов <baranov@semograph.com>
2020-03-29 00:39:16 +0500

    Исправляет вёрстку после ввода роутера

--- src/App.js
+++ src/App.js
@@ -8,31 +8,33 @@ import './App.css';
 
 function App() {
     return (
-        <HashRouter hashType="noslash">
+        <div>
+            <HashRouter hashType="noslash">
 
-            <div className="content">
-                <div className="top-menu">
-                    <ul className="menu">
-                      <li><a href="index.html">Music</a></li>
-                      <li><a href="about.html">About</a></li>
-                      <li><a href="signup.html">Sign-Up for updates</a></li>
-                      <li><a href="donate.html">Donate</a></li>
-                      <li><a href="feedback.html">Feedback</a></li>
-                    </ul>
+                <div className="content">
+                    <div className="top-menu">
+                        <ul className="menu">
+                          <li><a href="index.html">Music</a></li>
+                          <li><a href="about.html">About</a></li>
+                          <li><a href="signup.html">Sign-Up for updates</a></li>
+                          <li><a href="donate.html">Donate</a></li>
+                          <li><a href="feedback.html">Feedback</a></li>
+                        </ul>
+                    </div>
+
+                    <div className="logo">
+                        sleepingaliencreature
+                    </div>
+
+                    <Player/>
                 </div>
 
-                <div className="logo">
-                    sleepingaliencreature
-                </div>
+                <footer className="footer">
+                    <a href="#">Created by sleepingaliencreature 2020</a>
+                </footer>
 
-                <Player/>
-            </div>
-
-            <footer className="footer">
-                <a href="#">Created by sleepingaliencreature 2020</a>
-            </footer>
-
-        </HashRouter>
+            </HashRouter>
+        </div>
     );
 }
 
